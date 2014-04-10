5bc97bd4e602d8196c0fe1285d382d6b015eb530
Marcel Lutz <marcel@mlutz.ch>
2014-04-10 06:53:26 +0200

    Added class SourceRangeHyperlink to simplify hyperlink detectors.

---
+++ src/ch/mlutz/plugins/t4e/tapestry/editor/hyperlink/SourceRangeHyperlink.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Made in Switzerland, Marcel Lutz
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of The MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     Marcel Lutz - SourceRangeHyperlink
+ ******************************************************************************/
+package ch.mlutz.plugins.t4e.tapestry.editor.hyperlink;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import ch.mlutz.plugins.t4e.log.EclipseLogFactory;
+import ch.mlutz.plugins.t4e.log.IEclipseLog;
+import ch.mlutz.plugins.t4e.tools.EclipseTools;
+
+/**
+ * IHyperlink implementation that opens a file in an editor and possibly
+ * reveals a source range on click.
+ *
+ * @author Marcel Lutz
+ */
+public class SourceRangeHyperlink implements IHyperlink {
+
+	private static final IEclipseLog log=
+		EclipseLogFactory.create(SourceRangeHyperlink.class);
+
+	private IRegion hyperlinkRegion;
+
+	private String hyperlinkText;
+
+	private IFile targetFile;
+
+	private ISourceRange targetSourceRange;
+
+	private String editorId;
+
+	/**
+	 * Constructor
+	 *
+	 * @param hyperlinkRegion
+	 * @param hyperlinkText
+	 * @param targetFile
+	 * @param targetSourceRange
+	 * @param editorId
+	 */
+	public SourceRangeHyperlink(IRegion hyperlinkRegion, String hyperlinkText,
+			IFile targetFile, ISourceRange targetSourceRange, String editorId) {
+		super();
+		this.hyperlinkRegion = hyperlinkRegion;
+		this.hyperlinkText = hyperlinkText;
+		this.targetFile = targetFile;
+		this.targetSourceRange = targetSourceRange;
+		this.editorId = editorId;
+	}
+
+	@Override
+	public void open() {
+		IWorkbench wb= PlatformUI.getWorkbench();
+		IWorkbenchWindow win= wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page= win.getActivePage();
+
+		IEditorPart editorPart;
+		if (editorId != null) {
+			editorPart= EclipseTools.openFileInEditor(
+					targetFile, page, editorId);
+		} else {
+			editorPart= EclipseTools.openFileInEditor(
+					targetFile, page);
+		}
+
+		if (targetSourceRange != null) {
+			if (editorPart instanceof ITextEditor) {
+				((ITextEditor) editorPart).selectAndReveal(
+						targetSourceRange.getOffset(),
+						targetSourceRange.getLength());
+			} else {
+				log.info("editorPart !instanceof ITextEditor");
+			}
+		}
+	}
+
+	@Override
+	public String getTypeLabel() {
+		return null;
+	}
+
+	@Override
+	public String getHyperlinkText() {
+		return hyperlinkText;
+	}
+
+	@Override
+	public IRegion getHyperlinkRegion() {
+		return hyperlinkRegion;
+	}
+}
